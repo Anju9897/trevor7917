5cefdd38e819a34ab039916d3a56da0e933d8341
Georgina <57773093+issagranadosgarcia@users.noreply.github.com>
2019-11-25 12:12:59 -0600

    .

--- Trevor2.0/src/java/com/trevor/control/Vision.java
+++ Trevor2.0/src/java/com/trevor/control/Vision.java
@@ -1,11 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.trevor.control;
 
-<<<<<<< HEAD
 import com.trevor.entidad.Menu;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -20,42 +15,12 @@ import javax.servlet.http.HttpSession;
 
 @WebServlet (name = "Vision", urlPatterns = "{/Vision}")
 
-public class Vision extends HttpServlet {
-
-    
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        HttpSession s = request.getSession();
-        List<Menu> per = (List<Menu>) s.getAttribute("Permisos");
-        List<Menu> MenuPrincipal = per.stream().filter(field -> field.getIdpadre() == 0).collect(Collectors.toList());
-        request.setAttribute("MenuPrincipal", MenuPrincipal);
-        String op = request.getParameter("op");
-        if(op != null){
-         List<Menu> PermisosAsignados = per.stream().filter(field -> field.getIdpadre() == Integer.parseInt(op)).collect(Collectors.toList());
-            request.setAttribute("PermisosAsignados", PermisosAsignados);   
-        }
-        String accion = request.getParameter("accion");    
-        if (accion ==null){
-                request.getRequestDispatcher("Vision/Vision.jsp").forward(request, response);
-            }
-        }
-    
-=======
-import java.io.IOException;
-import java.io.PrintWriter;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 
 public class Vision extends HttpServlet {
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+   
        
     }
 
@@ -63,33 +28,33 @@ public class Vision extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+                HttpSession s = request.getSession();
+        List<Menu> per = (List<Menu>) s.getAttribute("Permisos");
+        List<Menu> MenuPrincipal = per.stream().filter(field -> field.getIdpadre() == 0).collect(Collectors.toList());
+        request.setAttribute("MenuPrincipal", MenuPrincipal);
+        String op = request.getParameter("op");
+        if (op != null) {
+            List<Menu> PermisosAsignados = per.stream().filter(field -> field.getIdpadre() == Integer.parseInt(op)).collect(Collectors.toList());
+            request.setAttribute("PermisosAsignados", PermisosAsignados);
+                   PrintWriter io = response.getWriter();
+        String accion = request.getParameter("accion");        
+        if (accion ==null){
+                request.getRequestDispatcher("Vision/Vision.jsp").forward(request, response);
+            }
+        }
+        
+        
     }
->>>>>>> 7e115a1027bf10552562cde371c28236d4129cc3
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-<<<<<<< HEAD
+  
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-=======
-        processRequest(request, response);
-    }
-
->>>>>>> 7e115a1027bf10552562cde371c28236d4129cc3
     @Override
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
 
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 7e115a1027bf10552562cde371c28236d4129cc3
