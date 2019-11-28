0230ce8d5e7d18bb24a2f920e042c6488dc67348
Juan Garcia <Anju9897@gmail.com>
2019-11-28 01:56:35 -0600

    por los historial

---
+++ Trevor2.0/src/java/com/trevor/control/Notificacion.java
@@ -0,0 +1,50 @@
+
+package com.trevor.control;
+
+import com.trevor.operaciones.Operaciones;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Notificacion extends HttpServlet {
+
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+  
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try{
+            Conexion conn = new ConexionPool();
+        }catch(Exception ex){
+            try {
+                Operaciones.rollback();
+            } catch (SQLException ex1) {
+                Logger.getLogger(Notificacion.class.getName()).log(Level.SEVERE, null, ex1);
+            }
+        }finally{
+            try {
+                Operaciones.cerrarConexion();
+            } catch (SQLException ex) {
+                Logger.getLogger(Notificacion.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
